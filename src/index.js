33b0118a1d2a73811317796fc0e907f3d6eb4ab6
Jamile Ramos <jamilersilva21@gmail.com>
2025-11-22 17:28:54 -0300

    criado rota pseudofrutos

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ server.listen(3000,()=>{
     console.log("Olá, mundo")
 
 });
+
+server.get("/pseudofrutos", (_,response) =>{
+    response.json([
+        {
+            name: "Morango",
+            type:"pseudofruto",
+
+        },
+        {
+            name:"Maçã",
+            type:"pseudofruto",
+            
+        }
+    ])
+
+
+
+})
